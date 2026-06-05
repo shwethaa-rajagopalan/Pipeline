447e3f0a9a2a2d30bd3c190e299e51431e3a9648
ShwethaaRajagopalan <shwethaarajagopalan@gmail.com>
2026-06-05 18:21:57 +0530

    Refactor Spark session creation to avoid local master configuration in remote contexts

--- utils/spark_session.py
+++ utils/spark_session.py
@@ -1,4 +1,5 @@
 from typing import Optional, Dict
+import os
 
 from pyspark.sql import SparkSession
 
@@ -14,7 +15,17 @@ def create_spark_session(app_name: str = "dbx_pipeline", configs: Optional[Dict[
         SparkSession
     """
     builder = SparkSession.builder.appName(app_name)
-    builder = builder.master("local[*]")
+
+    # Avoid configuring a local master when Spark Connect / remote is in use (Databricks Spark Connect).
+    # If Spark Connect is configured via environment (SPARK_REMOTE) or via provided configs
+    # (spark.remote), setting `master("local[*]")` will cause a startup validation error.
+    spark_connect_env = os.environ.get("SPARK_REMOTE") or os.environ.get("SPARK_CONNECT")
+    databricks_runtime = os.environ.get("DATABRICKS_RUNTIME_VERSION") or os.environ.get("DBR_RUNTIME_VERSION")
+    configs_have_remote = configs and ("spark.remote" in configs)
+
+    if not (spark_connect_env or databricks_runtime or configs_have_remote):
+        builder = builder.master("local[*]")
+
     if configs:
         for k, v in configs.items():
             builder = builder.config(k, v)
